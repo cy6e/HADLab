038a8b378bbfdc3a416041250528cd32a8efaa5e
cy6e <97270008+cy6e@users.noreply.github.com>
2024-01-09 23:07:47 +0530

    Update intent.java

--- intent.java
+++ intent.java
@@ -1,32 +1,95 @@
-EditText name ;
-Button click ;
-  click.setOnClickListener(new View.OnClickListener() {
-  @Override
-  public void onClick(View v) {
-  Intent it = new Intent(MainActivity.this, SecondActivity.class);
-  Bundle b = new Bundle() ;
-  b.putString("name",name.getText().toString());
-  it.putExtras(b) ;
-  startActivity(it);
-  }
-});
+package com.example.practice2;
 
-name = findViewById(R.id.name) ;
-click = findViewById(R.id.click);
+import androidx.appcompat.app.AppCompatActivity;
 
-//second activity
-TextView myName ;
-String nameFromFirstActivity =" ";
-//inside oncreate
-myName = findViewById(R.id.mytext) ;
-nameFromFirstActivity = getIntent().getStringExtra("name");
-myName.setText(nameFromFirstActivity);
-//<uses-permission android:name="android.permission.CALL_PHONE"/>
-call.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-Uri myUri = Uri.parse("tel:"+phoneNumberToCall.getText().toString());
-Intent it = new Intent(Intent.ACTION_DIAL, myUri) ;
-startActivity(it);
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name;
+    Button click;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        click = findViewById(R.id.click);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, MainActivity2.class);
+                Bundle b = new Bundle();
+                b.putString("name",name.getText().toString());
+                it.putExtras(b);
+                startActivity(it);
+            }
+        });
+    }
 }
-});
+
+package com.example.practice2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView mytext;
+    String FirstActivityName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mytext = findViewById(R.id.mytext);
+        FirstActivityName = getIntent().getStringExtra("name");
+        mytext.setText(FirstActivityName);
+    }
+}
+
+
+    <uses-feature
+        android:name="android.hardware.telephony"
+        android:required="false" />
+    <uses-permission android:name="android.permission.CALL_PHONE"/>
+
+
+package com.example.practice2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText phoneNumber;
+    Button click;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phoneNumber = findViewById(R.id.name);
+        click = findViewById(R.id.click);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri myUri = Uri.parse("tel:"+phoneNumber.getText().toString());
+                Intent it = new Intent(Intent.ACTION_DIAL, myUri);
+                startActivity(it);
+            }
+        });
+    }
+}
